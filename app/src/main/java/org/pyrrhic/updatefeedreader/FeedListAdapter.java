421b8298982b24f3a27706d17a3a7d3b75761156
bittercode <bittercode@gmail.com>
2015-03-27 10:17:19 +0100

    Cleaned up the adapter.

--- app/src/main/java/org/pyrrhic/updatefeedreader/FeedListAdapter.java
+++ app/src/main/java/org/pyrrhic/updatefeedreader/FeedListAdapter.java
@@ -57,16 +57,13 @@ public class FeedListAdapter
                     .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             convertView = mInflater.inflate(R.layout.item_feed,viewGroup,false);
         }
-        //TableLayout item_feed = (TableLayout) context.findViewById(R.id.item_feed);
 
         ImageView categoryImageView = (ImageView) convertView.findViewById(R.id.categoryImageView);
         TextView titleTextView = (TextView) convertView.findViewById(R.id.titleTextView);
         TextView dateTextView = (TextView) convertView.findViewById(R.id.dateTextView);
         TextView summaryTextView = (TextView) convertView.findViewById(R.id.summaryTextView);
 
-        //if (getItem(index) != null) {
         SyndEntry entry = getItem(index);
-        //}
 
         titleTextView.setText( entry.getTitle() );
         titleTextView.setTextColor(Color.BLUE);
@@ -89,8 +86,6 @@ public class FeedListAdapter
 
         return convertView;
 
-        //newsEntryCellView.display( index );
-        //return newsEntryCellView;
     }
 
     public void click( int position )
@@ -100,90 +95,4 @@ public class FeedListAdapter
         context.startActivity( webIntent );
     }
 
-    /*private class NewsEntryCellView
-        extends TableLayout
-    {
-        //private TextView titleTextView;
-        //private TextView dateTextView;
-        //private TextView summaryTextView;
-        //private TextView categoryTextView;
-        //private ImageView categoryImageView;
-
-        public NewsEntryCellView()
-        {
-            super( context );
-            createUI();
-        }
-
-        private void createUI()
-        {
-             setColumnShrinkable( 0, false );
-            setColumnStretchable( 0, false );
-            setColumnShrinkable( 1, false );
-            setColumnStretchable( 1, false );
-            setColumnShrinkable( 2, false );
-            setColumnStretchable( 2, true );
-            setColumnStretchable( 3, true);
-            setColumnShrinkable( 3, false );
-
-            setPadding( 10, 10, 10, 10 );
-
-            //TableLayout item_feed = (TableLayout)findViewById(R.id.item_feed);
-            //item_feed.setStretchAllColumns(true);
-
-            //TableRow row1 = new TableRow(context);
-
-            ImageView categoryImageView = (ImageView)context.findViewById(R.id.categoryImageView);
-
-            //row1.addView(categoryImageView);
-
-            TextView titleTextView = (TextView)context.findViewById(R.id.titleTextView);
-            //titleTextView.setPadding( 10, 10, 10, 10 );
-            //titleTextView.setTextColor(Color.RED);
-            //row1.addView(titleTextView);
-            //MainLayout.addView(row1);
-
-            //TableRow row2 = new TableRow(context);
-
-            TextView dateTextView = (TextView)context.findViewById(R.id.dateTextView);
-            //dateTextView.setPadding( 10, 10, 10, 10 );
-            //row2.addView( dateTextView );
-
-            TextView summaryTextView = (TextView)context.findViewById(R.id.summaryTextView);
-            //summaryTextView.setPadding( 10, 10, 10, 10 );
-            //row2.addView( summaryTextView );
-
-            //MainLayout.addView(row2);
-           // addView(item_feed);
-            //categoryTextView = new TextView( context );
-            //categoryTextView.setPadding(10,10,10,10);
-            //addView( categoryTextView );
-        }
-
-
-        public void display( int index )
-        {
-            SyndEntry entry = getItem( index );
-            titleTextView.setText( entry.getTitle() );
-            titleTextView.setTextColor(Color.BLUE);
-            dateTextView.setText( entry.getPublishedDate().toString() );
-            summaryTextView.setText( entry.getDescription().getValue() );
-            List<SyndCategory> categories = entry.getCategories();
-            SyndCategory category = categories.get(0); //there is only one
-            int catnum = Integer.parseInt(category.getName());
-            //String catname="";
-            switch (catnum) {
-                case 1: categoryImageView.setImageResource(R.drawable.praise); //Praise
-                    break;
-                case 2: categoryImageView.setImageResource(R.drawable.prayer); //Prayer
-                    break;
-                case 3: categoryImageView.setImageResource(R.drawable.update); //Update
-                    break;
-                case 4: categoryImageView.setImageResource(R.drawable.family); //Family
-                    break;
-            }
-
-            //categoryTextView.setText(catname);
-        }
-    } */
 }
